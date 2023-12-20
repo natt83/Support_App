67c5da15138e1d8a2abafdd2ec3e6f3aab8f0a25
Natt83~ <natimetal83@gmail.com>
2023-12-20 14:20:47 +0100

    template views

---
+++ supportApp/src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HomeVue from '@/components/Home.vue';
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
---
+++ supportApp/src/views/SupportFormView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import 
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
